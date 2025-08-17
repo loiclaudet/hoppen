92de7f32ce7571b0abea2dabd7aee16d111f38e7
larnakid <8344874+larnakid@users.noreply.github.com>
2025-08-17 07:53:21 +0300

    feat: add favicon

--- hoppen.js
+++ hoppen.js
@@ -65,6 +65,7 @@ function buildBaseHtml(title) {
     <title>${title}</title>
     <link rel="stylesheet" href="@@internal/reset.css" />
     <link rel="stylesheet" href="style.css" />
+    <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⧉</text></svg>">
   </head>
   <body>
   </body>
